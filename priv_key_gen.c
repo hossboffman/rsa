8438a5cd8266ba44480442752e01adc41e087b0b
Samuel Henshaw <55932235+hossboffman@users.noreply.github.com>
2019-10-10 17:02:22 -0400

    Create priv_key_gen.c

---
+++ priv_key_gen.c
@@ -0,0 +1,9 @@
+/*this is a program that creates the private key, consisting of a modulus n having two distinct
+ *large prime divisors and the private residue d which is coprime to the totient of n*/
+ 
+#include<stdio.h>
+#include<gmp.h>
+
+int main(void)
+{
+  
